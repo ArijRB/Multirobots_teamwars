405596683b60ca5789af6eabfc2aa30fa0210750
yannche <yann_che2@yahoo.fr>
2015-09-26 21:57:11 +0200

    setup now works with __init__.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 # ce fichier se compile avec la commande
 # python setup.py build_ext --inplace
 #
-
 from distutils.core import setup
-from distutils.extension import Extension
+from Cython.Distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 
-ext  =  [Extension( "rayon", sources=["rayon.py"] ,extra_compile_args=["-Wno-unused-function"])]
+ext  =  [Extension( "rayon", ["rayon.py"] ,\
+        cython_include_dirs=[''],\
+        extra_compile_args=["-Wno-unused-function"])]
 
 setup(
    cmdclass={'build_ext' : build_ext},
