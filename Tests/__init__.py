e36d25c9dd51ec449b479593886a0b8abcbe8fd5
yannche <yann_che2@yahoo.fr>
2016-03-12 20:00:49 +0100

    init.py

--- Tests/__init__.py
+++ Tests/__init__.py
@@ -5,8 +5,10 @@
 # Ajout du dossier pySpriteWorld dans le path
 import os,sys
 dossier = os.path.dirname(os.path.abspath(__file__))
-while not dossier.endswith('pySpriteWorld'):
-    dossier = os.path.dirname(dossier)
-dossier = os.path.dirname(dossier)
+
+#while not dossier.endswith('pySpriteWorld'):
+#    dossier = os.path.dirname(dossier)
+#dossier = os.path.dirname(dossier)
+
 if dossier not in sys.path:
     sys.path.append(dossier)
