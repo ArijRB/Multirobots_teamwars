f7f927e489247e8c303613efb5c23331a8dbed27
nekonaute <nicolas.bredeche@upmc.fr>
2017-03-30 13:54:50 +0200

    Added teamwars template

---
+++ teamwars.py
@@ -0,0 +1,345 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# multirobot_teamwars.py
+# Contact (ce fichier uniquement): nicolas.bredeche(at)upmc.fr
+# Ce code utilise pySpriteWorld, développé par Yann Chevaleyre (U. Paris 13)
+# 
+# Description:
+#   Template pour projet multi-robots "MULTIROBOT WARS"
+#       But du jeu: posséder le maximum de cases!
+#           Chaque joueur dispose de quatre robots
+#           Le monde est divisé en 1024 cases (ie. 32x32 cases de 16x16 pixels)
+#           Le jeu tourne pendant 4000 itérations
+#           Une case "appartient" à la dernière équipe qui l'a visitée
+#       Ce que vous avez le droit de faire:
+#           Vous ne pouvez modifier que la méthode step(.) de la classe AgentTypeA
+#           Les vitesses de translation et rotation maximales sont données par maxTranslationSpeed et maxRotationSpeed
+#           La distance maximale autorisée des senseurs est maxSensorDistance
+#       Recommandations:
+#           Conservez intact multirobot_teamwars.py (travaillez sur une copie!)
+#           Pour faire vos tests, vous pouvez aussi modifier (si vous le souhaitez) la méthode step() pour la classe AgentTypeB. Il ne sera pas possible de transmettre cette partie là lors de l'évaluation par contre.
+#           La manière dont vous construirez votre fonction step(.) est libre. Par exemple:
+#               code écrit à la main, code obtenu par un processus d'apprentissage ou d'optimisation préalable, etc.
+#               comportements individuels, collectifs, parasites (p.ex: bloquer l'adversaire), etc.
+#       Evaluation:
+#           Soutenance devant machine (par binome, 15 min.) lors de la dernière séance de TP (matin et après-midi)
+#               Vous devrez montrer votre résultat sur trois arènes inédites
+#               Vous devrez mettre en évidence la réutilisation des concepts vus en cours
+#               Vous devrez mettre en évidence les choix pragmatiques que vous avez du faire
+#               Assurez vous que la simple copie de votre fonctions step(.) dans le fichier multirobots_teamwars.py suffit pour pouvoir le tester
+#           Vous affronterez vos camarades
+#               Au tableau: une matrice des combats a mettre a jour en fonction des victoires et défaites
+#               Affrontement sur les trois arènes inédites
+#               vous pouvez utiliser http://piratepad.net pour échanger votre fonction step(.))
+#       Bon courage!
+# 
+# Dépendances:
+#   Python 2.x
+#   Matplotlib
+#   Pygame
+# 
+# Historique: 
+#   2016-03-28__23:23 - template pour 3i025 (IA&RO, UPMC, licence info)
+#
+# Aide: code utile
+#   - Partie "variables globales"
+#   - La méthode "step" de la classe Agent
+#   - La fonction setupAgents (permet de placer les robots au début de la simulation)
+#   - La fonction setupArena (permet de placer des obstacles au début de la simulation)
+#   - il n'est pas conseillé de modifier les autres parties du code.
+# 
+
+from robosim import *
+from random import random, shuffle
+import time
+import sys
+import atexit
+
+
+from teamwars_agentA import *  # EQUIPE A
+
+
+'''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''
+'''  variables globales   '''
+'''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''
+
+game = Game()
+agents = []
+
+sensors = []
+
+arena = 0
+
+nbAgents = 8 # doit être pair et inférieur a 32
+SensorBelt = [-170,-80,-40,-20,+20,40,80,+170]  # angles en degres des senseurs
+
+occupancyGrid = []
+for y in range(screen_height/16):
+    l = []
+    for x in range(screen_width/16):
+        l.append("_")
+    occupancyGrid.append(l)
+
+
+'''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''
+'''  Agent "B"            '''
+'''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''
+
+class AgentTypeB(object):
+    
+    agentIdCounter = 0 # use as static
+    id = -1
+    robot = -1
+
+    teamname = "B"
+    robotname = ""
+
+    def __init__(self,robot):
+        self.id = AgentTypeA.agentIdCounter
+        AgentTypeA.agentIdCounter = AgentTypeA.agentIdCounter + 1
+        self.name = str(self.teamname)+str(self.id)
+        self.robot = robot
+    
+    def getTeamname(self):
+        return self.teamname
+    
+    def getRobotname(self):
+        return self.robotname
+    
+    def getRobot(self):
+        return self.robot
+    
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # =-= JOUEUR B -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    teamname = "Equipe Test" # A modifier avec le nom de votre équipe
+
+    def step(self,sensors):
+
+        color( (0,0,255) )
+        circle( *self.getRobot().get_centroid() , r = 22) # je dessine un rond bleu autour de ce robot
+
+        #print "robot #", self.id, " -- step"
+
+        p = self.robot
+
+        # actions
+        sensor_infos = sensors[p] # sensor_infos est une liste de namedtuple (un par capteur).
+        #print "sensor_infos: ", sensor_infos[0].dist_from_border
+        distGauche = sensor_infos[2].dist_from_border
+        if distGauche > maxSensorDistance:
+            distGauche = maxSensorDistance # borne
+        distDroite = sensor_infos[5].dist_from_border
+        if distDroite > maxSensorDistance:
+            distDroite = maxSensorDistance # borne
+
+        if distGauche < distDroite:
+            self.setRotationValue( +1 )
+        elif distGauche > distDroite:
+            self.setRotationValue( -1 )
+        else:
+            self.setRotationValue( 0 )
+
+        self.setTranslationValue(1) # normalisé -1,+1
+
+        return
+
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+    def setTranslationValue(self,value):
+        if value > 1:
+            print "[WARNING] translation value not in [-1,+1]. Normalizing."
+            value = maxTranslationSpeed
+        elif value < -1:
+            print "[WARNING] translation value not in [-1,+1]. Normalizing."
+            value = -maxTranslationSpeed
+        else:
+            value = value * maxTranslationSpeed
+        self.robot.forward(value)
+
+    def setRotationValue(self,value):
+        if value > 1:
+            print "[WARNING] translation value not in [-1,+1]. Normalizing."
+            value = maxRotationSpeed
+        elif value < -1:
+            print "[WARNING] translation value not in [-1,+1]. Normalizing."
+            value = -maxRotationSpeed
+        else:
+            value = value * maxRotationSpeed
+        self.robot.rotate(value)
+
+
+'''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''
+'''  Fonctions init/step  '''
+'''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''
+
+
+def setupAgents():
+    global screen_width, screen_height, nbAgents, agents, game
+
+    # Make agents
+
+    nbAgentsTypeA = nbAgentsTypeB = nbAgents / 2
+    nbAgentsCreated = 0
+
+    for i in range(nbAgentsTypeA):
+        p = game.add_players( (16 , 200+32*i) , None , tiled=False)
+        p.oriente( 0 )
+        p.numero = nbAgentsCreated
+        nbAgentsCreated = nbAgentsCreated + 1
+        agents.append(AgentTypeA(p))
+
+    for i in range(nbAgentsTypeB):
+        p = game.add_players( (486 , 200+32*i) , None , tiled=False)
+        p.oriente( 180 )
+        p.numero = nbAgentsCreated
+        nbAgentsCreated = nbAgentsCreated + 1
+        agents.append(AgentTypeB(p))
+
+    game.mainiteration()
+
+
+
+def setupArena0():
+    for i in range(6,13):
+        addObstacle(row=3,col=i)
+    for i in range(3,10):
+        addObstacle(row=12,col=i)
+    addObstacle(row=4,col=12)
+    addObstacle(row=5,col=12)
+    addObstacle(row=6,col=12)
+    addObstacle(row=11,col=3)
+    addObstacle(row=10,col=3)
+    addObstacle(row=9,col=3)
+
+def setupArena1():
+    return
+
+def setupArena2():
+    for i in range(0,8):
+        addObstacle(row=i,col=7)
+    for i in range(8,16):
+        addObstacle(row=i,col=8)
+
+def stepWorld():
+
+    efface()
+        
+    # chaque agent se met à jour. L'ordre de mise à jour change à chaque fois (permet d'éviter des effets d'ordre).
+    shuffledIndexes = [i for i in range(len(agents))]
+    shuffle(shuffledIndexes)     ### TODO: erreur sur macosx
+    for i in range(len(agents)):
+        agents[shuffledIndexes[i]].step(sensors)
+        # met à jour la grille d'occupation
+        coord = agents[shuffledIndexes[i]].getRobot().get_centroid()
+        occupancyGrid[int(coord[0])/16][int(coord[1])/16] = agents[shuffledIndexes[i]].getTeamname() # first come, first served
+    return
+
+
+'''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''
+'''  Fonctions internes   '''
+'''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''
+
+def addObstacle(row,col):
+    # le sprite situe colone 13, ligne 0 sur le spritesheet
+    game.add_new_sprite('obstacle',tileid=(0,13),xy=(col,row),tiled=True)
+
+class MyTurtle(Turtle): # also: limit robot speed through this derived class
+    maxRotationSpeed = maxRotationSpeed # 10, 10000, etc.
+    def rotate(self,a):
+        mx = MyTurtle.maxRotationSpeed
+        Turtle.rotate(self, max(-mx,min(a,mx)))
+
+def displayOccupancyGrid():
+    global iteration
+    nbA = nbB = nothing = 0
+
+    for y in range(screen_height/16):
+        for x in range(screen_width/16):
+            if occupancyGrid[x][y] == AgentTypeA.teamname:
+                nbA = nbA+1
+                sys.stdout.write('A')
+            elif occupancyGrid[x][y] == AgentTypeB.teamname:
+                nbB = nbB+1
+                sys.stdout.write('B')
+            else:
+                nothing = nothing + 1
+                sys.stdout.write(occupancyGrid[x][y])
+        sys.stdout.write('\n')
+
+    sys.stdout.write('Time left: '+str(maxIterations-iteration)+'\n')
+    sys.stdout.write('Summary: \n')
+    sys.stdout.write('\t'+AgentTypeA.teamname+': ')
+    sys.stdout.write(str(nbA))
+    sys.stdout.write('\n')
+    sys.stdout.write('\t'+AgentTypeB.teamname+': ')
+    sys.stdout.write(str(nbB))
+    sys.stdout.write('\n')
+    sys.stdout.write('\tFree  : ')
+    sys.stdout.write(str(nothing))
+    sys.stdout.write('\n')
+    sys.stdout.flush() 
+
+    return nbA,nbB,nothing
+
+def onExit():
+    ret = displayOccupancyGrid()
+    print "\n\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+    if ret[0] > ret[1]:
+        print "Robots type A (\"" + str(AgentTypeA.teamname) + "\") wins!"
+    elif ret[0] < ret[1]:
+        print "Robots type B (\"" + str(AgentTypeB.teamname) + "\") wins!"
+    else: 
+        print "Nobody wins!"
+    print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n"
+    print "\n[Simulation::stop]"
+
+
+'''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''
+'''  Main loop            '''
+'''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''
+
+init('vide3',MyTurtle,screen_width,screen_height) # display is re-dimensioned, turtle acts as a template to create new players/robots
+game.auto_refresh = False # display will be updated only if game.mainiteration() is called
+game.frameskip = frameskip
+atexit.register(onExit)
+
+if arena == 0:
+    setupArena0()
+elif arena == 1:
+    setupArena1()
+else:
+    setupArena2()
+
+setupAgents()
+game.mainiteration()
+
+iteration = 0
+while iteration != maxIterations:
+    # c'est plus rapide d'appeler cette fonction une fois pour toute car elle doit recalculer le masque de collision,
+    # ce qui est lourd....
+    sensors = throw_rays_for_many_players(game,game.layers['joueur'],SensorBelt,max_radius = maxSensorDistance+game.player.diametre_robot() , show_rays=showSensors)
+    stepWorld()
+    game.mainiteration()
+    if iteration % 200 == 0:
+        displayOccupancyGrid()
+    iteration = iteration + 1
+
